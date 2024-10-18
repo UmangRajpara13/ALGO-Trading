d726bf8fbc8d70ff03202d493ddaf2d8b0af9d50
Umang Rajpara <umangrajpara99@gmail.com>
2024-10-18 15:44:23 +0530

    cssing

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,26 @@ import axios from 'axios';
 import CustomSlider from './Switch/Switch';
 
 
-function App() { 
+function App() {
 
   const tabData = useSelector((state) => state.tabs.tabData);
   const activeTab = useSelector((state) => state.tabs.activeTab);
 
   const dispatch = useDispatch()
 
- 
+
   useEffect(() => {
     axios.get('http://localhost:8000/hello')
-    .then((response) => {
+      .then((response) => {
         console.log(response); // Log the response data to the console
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.error('Error making API call:', error);
-    });
+      });
   }, [])
 
   return (
-    <div> 
+    <div className='container'>
       <div className='header'>
         <div className='company-name'>Volatility of Options</div>
         <div className="tab-buttons">
@@ -36,12 +36,13 @@ function App() {
               key={tab.id}
               onClick={() => dispatch(setActiveTab(tab.id))}
               className={activeTab === tab.id ? 'active' : ''}
+              style={{ borderRight: tab.id == 2 ? '0px' : '1px' }}
             >
               {tab.title}
             </button>
           ))}
         </div>
-        <CustomSlider/>
+        <CustomSlider />
         <div className='clock'> <Clock /></div>
       </div>
       <Tabs />
